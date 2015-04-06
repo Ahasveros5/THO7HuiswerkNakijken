69d15473bf3d45db95122c9d146cb53a6ddc0952
Pim Graafland <pim.graafland@live.nl>
2015-04-06 12:14:29 +0200

    shizzlemahdizzle

--- THO7HuiswerkNakijken/src/huiswerknakijken/hu/Servlets/LeerlingRegistratieServlet.java
+++ THO7HuiswerkNakijken/src/huiswerknakijken/hu/Servlets/LeerlingRegistratieServlet.java
@@ -1,10 +1,10 @@
 package huiswerknakijken.hu.Servlets;
 
+import huiswerknakijken.hu.DAO.PersonDAO;
 import huiswerknakijken.hu.Domain.Person;
 import huiswerknakijken.hu.Domain.Student;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -19,9 +19,8 @@ public class LeerlingRegistratieServlet {
 
 	@SuppressWarnings("null")
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		PrintWriter out = resp.getWriter();
 		resp.setContentType("text/html");
-
+		PersonDAO dao = new PersonDAO();
 		String naam = req.getParameter("invoer_naam");
 		String achternaam = req.getParameter("invoer_achternaam");
 		int studentnr = Integer.parseInt(req.getParameter("invoer_studentnr"));
@@ -42,14 +41,14 @@ public class LeerlingRegistratieServlet {
 			ww2.isEmpty()) {
 			
 			req.setAttribute("msgs", "Vul alle velden in.");
-			rd = req.getRequestDispatcher("register.jsp");
+			rd = req.getRequestDispatcher("LeerlingRegistreren.jsp");
 		} else {
 			if(!ww1.equals(ww2)) {
 				req.setAttribute("msgs", "Wachtwoorden komen niet overeen");
-				rd = req.getRequestDispatcher("register.jsp");
+				rd = req.getRequestDispatcher("LeerlingRegistreren.jsp");
 			} else if(!email1.equals(email2)){
 				req.setAttribute("msgs", "Emailadressen komen niet overeen");
-				rd = req.getRequestDispatcher("register.jsp");			
+				rd = req.getRequestDispatcher("LeerlingRegistreren.jsp");			
 			}
 		}
 		Person p = new Student();
@@ -57,8 +56,15 @@ public class LeerlingRegistratieServlet {
 		p.setPassword(ww1);
 		p.setFirstName(naam);
 		p.setLastName(achternaam);
-
+		p.setID(studentnr);
+		p.setRole(1);
 		
+		if(!dao.retrieveByEmail(email1, 0).getEmail().equals(null)){
+			req.setAttribute("msgs", "Emailadres staat al geregistreerd");
+			rd = req.getRequestDispatcher("LeerlingRegistreren.jsp");
+		}
+
+		dao.add(p);
 	}
 
 }
--- THO7HuiswerkNakijken/src/huiswerknakijken/hu/Servlets/LeraarRegistratieServlet.java
+++ THO7HuiswerkNakijken/src/huiswerknakijken/hu/Servlets/LeraarRegistratieServlet.java
@@ -1,5 +1,6 @@
 package huiswerknakijken.hu.Servlets;
 
+import huiswerknakijken.hu.DAO.PersonDAO;
 import huiswerknakijken.hu.Domain.Person;
 import huiswerknakijken.hu.Domain.Teacher;
 
@@ -25,7 +26,7 @@ public class LeraarRegistratieServlet extends HttpServlet {
 
 		String naam = req.getParameter("invoer_naam");
 		String achternaam = req.getParameter("invoer_achternaam");
-
+		PersonDAO dao = new PersonDAO();
 		String email1 = req.getParameter("invoer_email");
 		String email2 = req.getParameter("invoer_emailb");
 		String ww1 = req.getParameter("invoer_ww");
@@ -42,14 +43,14 @@ public class LeraarRegistratieServlet extends HttpServlet {
 			ww2.isEmpty()) {
 			
 			req.setAttribute("msgs", "Vul alle velden in.");
-			rd = req.getRequestDispatcher("register.jsp");
+			rd = req.getRequestDispatcher("LeraarRegistreren.jsp");
 		} else {
 			if(!ww1.equals(ww2)) {
 				req.setAttribute("msgs", "Wachtwoorden komen niet overeen");
-				rd = req.getRequestDispatcher("register.jsp");
+				rd = req.getRequestDispatcher("LeraarRegistreren.jsp");
 			} else if(!email1.equals(email2)){
 				req.setAttribute("msgs", "Emailadressen komen niet overeen");
-				rd = req.getRequestDispatcher("register.jsp");			
+				rd = req.getRequestDispatcher("LeraarRegistreren.jsp");			
 			}
 		}
 		
@@ -58,7 +59,14 @@ public class LeraarRegistratieServlet extends HttpServlet {
 		p.setLastName(achternaam);
 		p.setEmail(email1);
 		p.setPassword(ww1);
-		
+		p.setRole(2);
+	
+		if(!dao.retrieveByEmail(email1, 0).getEmail().equals(null)){
+			req.setAttribute("msgs", "Emailadres staat al geregistreerd");
+			rd = req.getRequestDispatcher("LeraarRegistreren.jsp");
+		}
+
+		dao.add(p);
 	}
 
 }
