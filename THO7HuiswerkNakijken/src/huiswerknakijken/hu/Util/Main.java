391d31309ca94bd29d93afdbe8cba7435ab76b37
Guido Kerkhove <guido_rulz@hotmail.com>
2015-04-06 13:48:00 +0200

    small

--- THO7HuiswerkNakijken/src/huiswerknakijken/hu/Util/Main.java
+++ THO7HuiswerkNakijken/src/huiswerknakijken/hu/Util/Main.java
@@ -1,20 +1,15 @@
 package huiswerknakijken.hu.Util;
-import huiswerknakijken.hu.DAO.ClassDAO;
 import huiswerknakijken.hu.DAO.PersonDAO;
-import huiswerknakijken.hu.Domain.Class;
-import huiswerknakijken.hu.Domain.Person.UserRole;
-import huiswerknakijken.hu.Domain.Student;
+import huiswerknakijken.hu.Domain.Person;
 
 public class Main {
 
 	
 	public static void main(String [ ] args)
 	{
-		ClassDAO cDAO = new ClassDAO();
-		Class s = cDAO.retrieveByName("IV2a", 2);
-		for (Student st : s.getStudents())
-			System.out.println(st.getEmail());
-		
+		PersonDAO pDAO = new PersonDAO();
+		Person p = pDAO.retrieveByEmail("Test@test.nl", 0);
+		System.out.println(p.getFirstName());
 		
 	}
 }
