cabb2fd38fae9d9a63243b5aa88d85ce91b17796
Guido Kerkhove <guido_rulz@hotmail.com>
2015-04-06 14:25:11 +0200

    no message

--- src/huiswerknakijken/hu/Util/Main.java
+++ src/huiswerknakijken/hu/Util/Main.java
@@ -1,11 +1,14 @@
 package huiswerknakijken.hu.Util;
 
+import huiswerknakijken.hu.DAO.PersonDAO;
+
 public class Main {
 
 	
 	public static void main(String [ ] args)
 	{
-		OracleConnectionPool.getConnection();
+		PersonDAO pDAO = new PersonDAO();
+		pDAO.retrieveByEmail("Test@test.nl", 0);
 		
 	}
 }
