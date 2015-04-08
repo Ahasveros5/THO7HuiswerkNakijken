eb29d505033575c502f99f96c42c7d5b57f4999a
Dudeim <Guido_rulz@hotmail.com>
2015-04-08 12:41:19 +0200

    no message

--- src/huiswerknakijken/hu/DAO/PersonDAO.java
+++ src/huiswerknakijken/hu/DAO/PersonDAO.java
@@ -236,8 +236,6 @@ public class PersonDAO implements DAOInterface<Person> {
 
 		return retrievedStudent;
 	}
-<<<<<<< HEAD:src/huiswerknakijken/hu/DAO/PersonDAO.java
-=======
 	
 	public ArrayList<Person> retrieveAllByClass(int classID, int layerLevel) {
 		Connection connection = OracleConnectionPool.getConnection();
@@ -255,7 +253,6 @@ public class PersonDAO implements DAOInterface<Person> {
 
 		return eU;
 	}
->>>>>>> origin/master:src/huiswerknakijken/hu/DAO/PersonDAO.java
 
 	/*public Person retrieveByStudentname(String s, int layerLevel) {
 		Person retrievedStudent = null;
--- src/huiswerknakijken/hu/Servlets/LoginServlet.java
+++ src/huiswerknakijken/hu/Servlets/LoginServlet.java
@@ -1,5 +1,8 @@
 package huiswerknakijken.hu.Servlets;
 
+import huiswerknakijken.hu.DAO.PersonDAO;
+import huiswerknakijken.hu.Domain.Person;
+
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
@@ -7,7 +10,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 public class LoginServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -15,16 +17,23 @@ public class LoginServlet extends HttpServlet {
 
 		String userEmail = req.getParameter("email_login");
 		String password = req.getParameter("ww_login");
+		PersonDAO pdao = new PersonDAO();
+		Person p = pdao.retrieveByEmail(userEmail, 1);
 		
 		RequestDispatcher rd = null;
 		//HttpSession session = null;
 		if(userEmail.isEmpty() || password.isEmpty()) {
 			req.setAttribute("msgs", "Vul alle velden in.");
 			rd = req.getRequestDispatcher("loginpage.jsp");
-				} else {				
+				} else {			
+					if (p != null || password.equals(p.getPassword())){
 					//session.setAttribute("userEmail",userEmail );
-					rd = req.getRequestDispatcher("succes.jsp");
-					}				
+					rd = req.getRequestDispatcher("index.jsp");
+					} else {
+						req.setAttribute("msgs", "Wachtwoord of gebruikersnaam incorrect.");
+						rd = req.getRequestDispatcher("loginpage.jsp");
+					}
+				}				
 		
 		if(rd != null) {
 			rd.forward(req, resp);
