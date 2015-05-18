18e566e3540d4c78f9d0e0ee77b350b2d2b8120f
Guido Kerkhove <guido_rulz@hotmail.com>
2015-05-18 11:54:27 +0200

    no message

--- src/huiswerknakijken/hu/Util/OracleConnection.java
+++ src/huiswerknakijken/hu/Util/OracleConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
  
 public class OracleConnection {
 	private static OracleConnection instance = new OracleConnection();
-	private static final String url = "jdbc:oracle:thin:@ondora01.hu.nl:8521:cursus01";
+	private static final String url = "jdbc:oracle:thin:@ondora02.hu.nl:8521:cursus02";
 	private static final String username = "THO_SOA_2015_TEAM1";
 	private static final String password = "THO_SOA_2015_TEAM1";
 	
